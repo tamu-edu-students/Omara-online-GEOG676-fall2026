a92d84b69bc0588ce0af90caff89b3bccd7bdcbc
Liam O'Mara <liamomara03@gmail.com>
2026-09-16 13:20:53 -0500

    Remove arcpy test file

--- import arcpy.py
+++
@@ -1,8 +0,0 @@
-import arcpy
-
-print("Python works!")
-print("ArcPy works!")
-
-print("ArcGIS Pro version:", arcpy.GetInstallInfo()["Version"])
-print("Product:", arcpy.GetInstallInfo()["ProductName"])
-print("Current workspace:", arcpy.env.workspace)
